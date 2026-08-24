f32ecf76b8ceff63cb6d7ad36c3e317d78d7cd1b
jdc4444 <jdc4444@users.noreply.github.com>
2026-08-24 17:29:38 +0700

    Publish reversible JDC adaptive video pilot

---
+++ README.md
@@ -0,0 +1,8 @@
+# JDC adaptive video pilot
+
+Public, reversible delivery test for eight homepage clips. The portfolio remains
+hosted and managed in Squarespace; this repository serves only the temporary HLS
+pilot assets used behind the `jdc-video-pilot=1` switch.
+
+No source masters, local filesystem paths, build manifests, or credentials are
+published here.
---
+++ routes.js
@@ -0,0 +1,10 @@
+window.JDC_VIDEO_PILOT_ROUTES = Object.freeze({
+  "04a6e835-4333-44c8-9371-fe856603b3c0": "media/04a6e835-4333-44c8-9371-fe856603b3c0/master.m3u8",
+  "0e764c18-3c49-4820-8dd1-e05116de7c4d": "media/0e764c18-3c49-4820-8dd1-e05116de7c4d/master.m3u8",
+  "1afd7cf7-f7eb-4345-9614-e989d5ea7aac": "media/1afd7cf7-f7eb-4345-9614-e989d5ea7aac/master.m3u8",
+  "9340c0e0-12c9-4ca7-a305-149c68f10acf": "media/9340c0e0-12c9-4ca7-a305-149c68f10acf/master.m3u8",
+  "c07a7957-7369-4a2e-9e9d-a67314fcea3a": "media/c07a7957-7369-4a2e-9e9d-a67314fcea3a/master.m3u8",
+  "df93c27e-5dd5-4b58-997d-6664af8af62d": "media/df93c27e-5dd5-4b58-997d-6664af8af62d/master.m3u8",
+  "edc2639e-d736-4107-8580-f3e1a864c81e": "media/edc2639e-d736-4107-8580-f3e1a864c81e/master.m3u8",
+  "f3502f3d-b76f-4b9c-96a1-d78ccf642691": "media/f3502f3d-b76f-4b9c-96a1-d78ccf642691/master.m3u8"
+});
